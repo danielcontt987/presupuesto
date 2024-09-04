02c40aa87299554b8e8ea716cf700597216298ce
Daniel_Contreras98 <jose.contreras@gremti.com>
2024-09-04 00:38:45 -0600

    Agregando migraciones para inventario

---
+++ app/Models/InventoryDetail.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class InventoryDetail extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'product_id',
+        'inventory_id',
+        'stock',
+        'min',
+        'max',
+    ];
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);    
+    }
+}
---
+++ database/migrations/2024_09_04_062742_create_inventory_details_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('inventory_details', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+            $table->unsignedBigInteger('inventory_id');
+            $table->foreign('inventory_id')->references('id')->on('inventories');
+            $table->decimal('stock')->default(0);
+            $table->decimal('min')->default(0);
+            $table->decimal('max')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('inventory_details');
+    }
+};
